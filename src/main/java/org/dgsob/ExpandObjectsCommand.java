4618a016f9c448406f53ba6461c8dc89d05cbc25
dgsob <dsobecki@uni-koeln.de>
2023-08-04 14:00:40 +0200

    Simplifying code, getting rid of unnecessary features

--- src/main/java/org/dgsob/ExpandObjectsCommand.java
+++ src/main/java/org/dgsob/ExpandObjectsCommand.java
@@ -14,13 +14,12 @@ import qupath.lib.objects.PathObject;
 import qupath.lib.objects.PathObjects;
 import qupath.lib.objects.hierarchy.PathObjectHierarchy;
 import qupath.lib.plugins.parameters.ParameterList;
-import qupath.lib.roi.ROIs;
 import qupath.lib.roi.interfaces.ROI;
 import qupath.lib.regions.ImagePlane;
 import qupath.lib.roi.GeometryTools;
 
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ExpandObjectsCommand {
@@ -32,30 +31,25 @@ public class ExpandObjectsCommand {
 
         ImageServer<BufferedImage> server = imageData.getServer();
 
-        Rectangle bounds = new Rectangle(0, 0, server.getWidth(), server.getHeight());
-
         PathObjectHierarchy hierarchy = imageData.getHierarchy();
 
         Collection<PathObject> pathObjects = getSelected(hierarchy);
 
         assert pathObjects != null;
 
+        ArrayList<PathObject> newObjects = new ArrayList<>();
+
         int objectsNumber = pathObjects.size();
         if(objectsNumber>100)
-            Dialogs.showInfoNotification("EtLMD notification", "You've chosen " + objectsNumber + " objects. This may take a while.");
+            Dialogs.showInfoNotification("EtLMD notification", "You've chosen " + objectsNumber + " objects.");
 
         ParameterList params = new ParameterList()
                 .addDoubleParameter("radiusMicrons", "Expansion radius", 3, GeneralTools.micrometerSymbol(), "Distance to expand ROI")
-                .addBooleanParameter("removeOriginal", "Delete original object", false, "Create annotation containing only the expanded region, with the original ROI removed")
-                .addBooleanParameter("constrainToParent", "Constrain to parent", false, "Constrain ROI to fit inside the ROI of the parent object")
                 ;
 
         boolean confirmed = Dialogs.showConfirmDialog("Expand selected", new ParameterPanelFX(params).getPane());
 
         if(confirmed) {
-            boolean constrainToParent = params.getBooleanParameterValue("constrainToParent");
-            boolean removeOriginal = params.getBooleanParameterValue("removeOriginal");
-
             double radiusPixels;
             PixelCalibration cal = server.getPixelCalibration();
             if (cal.hasPixelSizeMicrons())
@@ -66,42 +60,17 @@ public class ExpandObjectsCommand {
             for (PathObject pathObject : pathObjects) {
 
                 ROI roi = pathObject.getROI();
-
                 Geometry geometry = roi.getGeometry();
-
                 Geometry geometry2 = BufferOp.bufferOp(geometry, radiusPixels, BufferParameters.DEFAULT_QUADRANT_SEGMENTS);
-
-                // If the radius is negative (i.e. an erosion), then the parent will be the original object itself
-                boolean isErosion = radiusPixels < 0;
-                PathObject parent = isErosion ? pathObject : pathObject.getParent();
-                if (constrainToParent && !isErosion) {
-                    Geometry parentShape;
-                    if (parent == null || parent.getROI() == null)
-                        parentShape = ROIs.createRectangleROI(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight(), ImagePlane.getPlane(roi)).getGeometry();
-                    else
-                        parentShape = parent.getROI().getGeometry();
-                    geometry2 = geometry2.intersection(parentShape);
-                }
-
                 ROI roi2 = GeometryTools.geometryToROI(geometry2, ImagePlane.getPlane(roi));
-
-                // Create a new object, with properties based on the original
-                // TODO: check in geojson properties of DetectionObject, maybe change createDetectionObject to CellObject with null nuclei or something else
                 PathObject detection2 = PathObjects.createDetectionObject(roi2, pathObject.getPathClass());
                 detection2.setName(pathObject.getName());
                 detection2.setColor(pathObject.getColor());
-
-                if (constrainToParent || isErosion)
-                    hierarchy.addObjectBelowParent(parent, detection2, true);
-                else
-                    hierarchy.addObject(detection2);
+                newObjects.add(detection2);
+                hierarchy.addObjects(newObjects);
             }
-            if (removeOriginal) {
                 hierarchy.removeObjects(pathObjects, false);
                 hierarchy.getSelectionModel().clearSelection();
-            } else {
-                hierarchy.getSelectionModel().clearSelection();
-            }
         }
 
     }
